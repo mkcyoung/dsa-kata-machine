545989e1fd1f6b994b6dcd8e05e1a58f501a85a4
Michael Young <michael@builtforteams.com>
2024-04-09 06:51:08 +0100

    Tree traversal

--- src/day1/BTPostOrder.ts
+++ src/day1/BTPostOrder.ts
@@ -1,3 +1,21 @@
-export default function post_order_search(head: BinaryNode<number>): number[] {
+function walk(curr: BinaryNode<number> | null, path: number[]): number[] {
+    // Base case
+    // No more children
+    if (!curr) return path;
 
+    // pre
+    // nothing here
+
+    // recurse
+    walk(curr.left, path);
+    walk(curr.right, path);
+
+    // post
+    path.push(curr.value);
+
+    return path;
+}
+
+export default function post_order_search(head: BinaryNode<number>): number[] {
+    return walk(head, []);
 }
--- src/day1/BTPreOrder.ts
+++ src/day1/BTPreOrder.ts
@@ -1,3 +1,22 @@
-export default function pre_order_search(head: BinaryNode<number>): number[] {
+// Tip: Whenever doing recursion - always make a second function
+function walk(curr: BinaryNode<number> | null, path: number[]): number[] {
+    // Base case
+    // No more children
+    if (!curr) return path;
 
+    // pre
+    path.push(curr.value);
+
+    // recurse
+    walk(curr.left, path);
+    walk(curr.right, path);
+
+    // post
+    // no post here
+
+    return path;
+}
+
+export default function pre_order_search(head: BinaryNode<number>): number[] {
+    return walk(head, []);
 }
