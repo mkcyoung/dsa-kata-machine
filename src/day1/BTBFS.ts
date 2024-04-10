69e0267c5f9254c9f4f79fffc3cd11c6d04f2a6e
Michael Young <michael@builtforteams.com>
2024-04-10 08:03:21 +0100

    Implement BFS

--- src/day1/BTBFS.ts
+++ src/day1/BTBFS.ts
@@ -1,3 +1,18 @@
+// We don't need to use recursion!
 export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+    // Pretending this is a queue - remember that array lists have O(n) time for shift/unshift
+    const q = [head];
 
+    while (q.length) {
+        // Take the first element out.
+        const curr = q.shift();
+        if (!curr) return false;
+        // Return true if we've found it.
+        if (curr.value === needle) return true;
+        // Otherwise, push its children to the queue from left to right.
+        if (curr.left) q.push(curr.left);
+        if (curr.right) q.push(curr.right);
+    }
+
+    return false;
 }
