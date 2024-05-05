e80bf19a7138ee895b90bf72d85d31d5aa45d2a5
Michael Young <michael@builtforteams.com>
2024-05-05 10:02:57 +0200

    DFS graph list

--- src/day1/DFSGraphList.ts
+++ src/day1/DFSGraphList.ts
@@ -1,3 +1,47 @@
-export default function dfs(graph: WeightedAdjacencyList, source: number, needle: number): number[] | null {
+// VERY similar to the MazeSolver
+function walk(
+    graph: WeightedAdjacencyList,
+    curr: number,
+    needle: number,
+    seen: boolean[],
+    path: number[],
+): boolean {
+    // BASE CASES!!
+    if (curr === needle) {
+        // Ensures that we capture the last node in the path.
+        path.push(curr);
+        return true;
+    }
 
+    if (seen[curr]) return false;
+
+    // Pre
+    seen[curr] = true;
+    path.push(curr);
+
+    // Recurse
+    const list = graph[curr];
+    for (let i = 0; i < list.length; i++) {
+        const edge = list[i];
+        if (walk(graph, edge.to, needle, seen, path)) {
+            return true;
+        }
+    }
+
+    // Post
+    path.pop();
+
+    return false;
+}
+
+export default function dfs(
+    graph: WeightedAdjacencyList,
+    source: number,
+    needle: number,
+): number[] | null {
+    const seen = new Array(graph.length).fill(false);
+    const path: number[] = [];
+    walk(graph, source, needle, seen, path);
+    if (path.length === 0) return null;
+    return path;
 }
